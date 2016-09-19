b4b9b8bf48526d79bcf97841fade98fe570bf697
Amber <amber@amberwilkie.com>
2016-09-19 17:51:35 +0200

    Add logic for entire silly cooper table

--- spec/CooperSpec.js
+++ spec/CooperSpec.js
@@ -16,32 +16,272 @@ describe("Cooper", function() {
         expect(person.gender).toEqual('female');
     });
 
-    describe('Calculate results', function() {
-        beforeEach(function() {
-            person = new Person({
-                gender: 'female',
-                age: 13
+    describe('Calculate results - ladies!', function() {
+
+        describe('Results for 13-year-old female', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'female',
+                    age: 13
+                });
             });
-        });
 
-        describe('Results for 13-year-old female', function(){
-
-            it('2100 should be Excellent', function() {
+            it('2100 should be Excellent', function () {
                 expect(person.getCooperForWomen(2100)).toBe('Excellent')
             });
 
-            it('1950 should be Above Average', function() {
+            it('1950 should be Above Average', function () {
                 expect(person.getCooperForWomen(1950)).toBe('Above average')
             });
 
-            it('1700 should be Average', function() {
+            it('1700 should be Average', function () {
                 expect(person.getCooperForWomen(1700)).toBe('Average')
             });
 
-            it('1400 should be Poor', function() {
+            it('1550 should be Average', function () {
+                expect(person.getCooperForWomen(1550)).toBe('Below average')
+            });
+
+            it('1400 should be Poor', function () {
                 expect(person.getCooperForWomen(1400)).toBe('Poor')
             });
-        })
-    })
+        });
+
+        describe('Results for 15yr female', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'female',
+                    age: 15
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForWomen(2200)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForWomen(1500)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 17yr female', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'female',
+                    age: 17
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForWomen(2400)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForWomen(1600)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 20yr female', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'female',
+                    age: 20
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForWomen(2700)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForWomen(1400)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 30yr female', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'female',
+                    age: 30
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForWomen(2600)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForWomen(1300)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 40yr female', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'female',
+                    age: 40
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForWomen(2400)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForWomen(1100)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 50yr female', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'female',
+                    age: 50
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForWomen(2300)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForWomen(1000)).toBe('Poor')
+            });
+        });
+    });
+
+    describe('Calculate results - mens :(', function() {
+
+        describe('Results for 13-year-old male', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'male',
+                    age: 13
+                });
+            });
+
+            it('2800 should be Excellent', function () {
+                expect(person.getCooperForMen(2800)).toBe('Excellent')
+            });
+
+            it('2600 should be Above Average', function () {
+                expect(person.getCooperForMen(2600)).toBe('Above average')
+            });
+
+            it('2200 should be Average', function () {
+                expect(person.getCooperForMen(2200)).toBe('Average')
+            });
+
+            it('2100 should be Average', function () {
+                expect(person.getCooperForMen(2100)).toBe('Below average')
+            });
+
+            it('2000 should be Poor', function () {
+                expect(person.getCooperForMen(2000)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 15yr male', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'male',
+                    age: 15
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForMen(2900)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForMen(2100)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 17yr male', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'male',
+                    age: 17
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForMen(3100)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForMen(2100)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 20yr male', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'male',
+                    age: 20
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForMen(2850)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForMen(1500)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 30yr male', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'male',
+                    age: 30
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForMen(2800)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForMen(1300)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 40yr male', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'male',
+                    age: 40
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForMen(2600)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForMen(1100)).toBe('Poor')
+            });
+        });
+
+        describe('Results for 50yr male', function () {
+            beforeEach(function () {
+                person = new Person({
+                    gender: 'male',
+                    age: 50
+                });
+            });
+
+            it('should be Excellent', function () {
+                expect(person.getCooperForMen(2500)).toBe('Excellent')
+            });
+
+            it('should be Poor', function () {
+                expect(person.getCooperForMen(1000)).toBe('Poor')
+            });
+        });
+    });
 
 });
--- src/cooper.js
+++ src/cooper.js
@@ -9,6 +9,108 @@ Number.prototype.between = function(a, b) {
     return this >= min && this <= max;
 };
 
+Person.prototype.getCooperForMen = function(distance) {
+    var age = this.age;
+    var ageGroup = getAgeGroup(age);
+    var cooperMessage;
+
+    switch (ageGroup) {
+        case '13-14':
+            if (distance >= 2700) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2400, 2699)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(2200, 2399)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(2100, 2199)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '15-16':
+            if (distance >= 2800) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2500, 2799)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(2300, 2499)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(2200, 2299)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '17-19':
+            if (distance >= 3000) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2700, 2999)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(2500, 2699)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(2300, 2499)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '20-29':
+            if (distance >= 2800) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2400, 2799)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(2200, 2399)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1600, 2199)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '30-39':
+            if (distance >= 2700) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2300, 2699)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(2000, 2299)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1400, 1999)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '40-49':
+            if (distance >= 2500) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2100, 2499)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1700, 2099)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1400, 1699)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '50+':
+            if (distance >= 2400) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2000, 2399)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1600, 1999)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1300, 1599)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+    }
+    return cooperMessage;
+
+};
+
 Person.prototype.getCooperForWomen = function(distance) {
     var age = this.age;
     var ageGroup = getAgeGroup(age);
@@ -16,7 +118,7 @@ Person.prototype.getCooperForWomen = function(distance) {
 
     switch (ageGroup) {
         case '13-14':
-            if (distance > 2000) {
+            if (distance >= 2000) {
                 cooperMessage = messages(1);
             } else if (distance.between(1900, 1999)) {
                 cooperMessage = messages(2);
@@ -27,7 +129,85 @@ Person.prototype.getCooperForWomen = function(distance) {
             } else {
                 cooperMessage = messages(5);
             }
-        break;
+            break;
+        case '15-16':
+            if (distance >= 2100) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2000, 2099)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1700, 1999)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1600, 1699)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '17-19':
+            if (distance >= 2300) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2100, 2299)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1800, 2099)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1700, 1799)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '20-29':
+            if (distance >= 2700) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2200, 2699)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1800, 2199)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1500, 1799)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '30-39':
+            if (distance >= 2500) {
+                cooperMessage = messages(1);
+            } else if (distance.between(2000, 2499)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1700, 1999)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1400, 1699)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '40-49':
+            if (distance >= 2300) {
+                cooperMessage = messages(1);
+            } else if (distance.between(1900, 2299)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1500, 1899)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1200, 1499)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
+        case '50+':
+            if (distance >= 2200) {
+                cooperMessage = messages(1);
+            } else if (distance.between(1700, 2199)) {
+                cooperMessage = messages(2);
+            } else if (distance.between(1400, 1699)) {
+                cooperMessage = messages(3);
+            } else if (distance.between(1100, 1399)) {
+                cooperMessage = messages(4);
+            } else {
+                cooperMessage = messages(5);
+            }
+            break;
     }
     return cooperMessage;
 
@@ -38,6 +218,16 @@ getAgeGroup = function(age) {
       return '13-14'
   }  else if (age.between(15, 16)){
       return '15-16'
+  }  else if (age.between(17, 19)){
+      return '17-19'
+  }  else if (age.between(20, 29)){
+      return '20-29'
+  }  else if (age.between(30, 39)){
+      return '30-39'
+  }  else if (age.between(40, 49)){
+      return '40-49'
+  }  else if (age >= 50){
+      return '50+'
   } else {
       return 'no age'
   }
